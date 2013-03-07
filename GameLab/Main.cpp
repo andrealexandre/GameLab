a6e3afff7b489958b1fec3762e79dac086761671
andrealexandre <andrealexandre316@gmail.com>
2013-03-07 00:00:00 +0000

    rotating cube added, usage of index buffer.

--- GameLab/Main.cpp
+++ GameLab/Main.cpp
@@ -19,6 +19,7 @@
 LPDIRECT3D9 d3d;			// the pointer to our Direct3D interface
 LPDIRECT3DDEVICE9 d3ddev;	// the pointer to the device class
 LPDIRECT3DVERTEXBUFFER9 v_buffer = NULL; // the pointer to the vertex buffer
+LPDIRECT3DINDEXBUFFER9 i_buffer = NULL; // the pointer to the index buffer
 
 // function prototypes
 void InitD3D(HWND hWnd); // sets up to our Direct3D interface
@@ -199,22 +200,23 @@ void Render_Frame(void)
 
 		d3ddev->SetTransform(D3DTS_PROJECTION, &matProjection); //set projection
 
-		//select the vertex buffer to the back buffer
+		//select the vertex and index buffers to use
 		d3ddev->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
+		d3ddev->SetIndices(i_buffer);
 
 		D3DXMATRIX matTranslateA; // a matrix to store the translation for triangle A
 		D3DXMATRIX matTranslateB; // a matrix to store the translation for triangle B		
 		D3DXMATRIX matRotateY; // a matrix to store the rotation for each triangle		
 		static float index = 0.0f; index += 0.05f; // an ever-increasing float value
 		
-		// build MULTIPLE matrices to translate the model and one to rotate
-		D3DXMatrixTranslation(&matTranslateA, 0.0f, 0.0f, 2.0f);
-		D3DXMatrixTranslation(&matTranslateB, 0.0f, 0.0f, -2.0f);		
+		// build MULTIPLE matrices to translate the model and one to rotate		
 		D3DXMatrixRotationY(&matRotateY, -index); // the front side
 		
 		// tell Direct3D about each world transform, and then draw another triangle
-		d3ddev->SetTransform(D3DTS_WORLD, &(matTranslateA * matRotateY));
-		d3ddev->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
+		d3ddev->SetTransform(D3DTS_WORLD, &matRotateY);
+
+		//d3ddev->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
+		d3ddev->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 8, 0, 12);
 
 		//d3ddev->SetTransform(D3DTS_WORLD, &(matTranslateB * matRotateY));
 		//d3ddev->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
@@ -238,10 +240,14 @@ void Init_Graphics(void)
 	// create the vertices using the CUSTOMVERTEX struct
 	CUSTOMVERTEX t_vert[] =
 	{
-		{ -3.0f, 3.0f, 0.0f, D3DCOLOR_XRGB(0, 0, 255) },
-		{ 3.0f, 3.0f, 0.0f, D3DCOLOR_XRGB(0, 255, 0) },
-		{ -3.0f, -3.0f, 0.0f, D3DCOLOR_XRGB(255, 0, 0) },
-		{ 3.0f, -3.0f, 0.0f, D3DCOLOR_XRGB(0, 255, 255) }
+		{ -3.0f, 3.0f, -3.0f, D3DCOLOR_XRGB(0, 0, 255) },	// vertex 0
+		{ 3.0f, 3.0f, -3.0f, D3DCOLOR_XRGB(0, 255, 0) },	// vertex 1
+		{ -3.0f, -3.0f, -3.0f, D3DCOLOR_XRGB(255, 0, 0) },	// 2
+		{ 3.0f, -3.0f, -3.0f, D3DCOLOR_XRGB(0, 255, 255) },	// 3
+		{ -3.0f, 3.0f, 3.0f, D3DCOLOR_XRGB(0, 0, 255) },	// ...
+		{ 3.0f, 3.0f, 3.0f, D3DCOLOR_XRGB(255, 0, 0) },
+		{ -3.0f, -3.0f, 3.0f, D3DCOLOR_XRGB(0, 255, 0) },
+		{ 3.0f, -3.0f, 3.0f, D3DCOLOR_XRGB(0, 255, 255) }
 	};
 
 	// create a vertex buffer interface called v_buffer
@@ -258,4 +264,34 @@ void Init_Graphics(void)
 	v_buffer->Lock(0,0, (void**) &pVoid, 0);
 	memcpy(pVoid, t_vert, sizeof(t_vert));
 	v_buffer->Unlock();
+
+	// create the indices using an int array
+	short indices[] =
+	{
+		0, 1, 2,	// side 1
+		2, 1, 3,		
+		4, 0, 6,	// side 2		
+		6, 0, 2,
+		7, 5, 6,	// side 3
+		6, 5, 4,
+		3, 1, 7,	// side 4
+		7, 1, 5,
+		4, 5, 0,	// side 5
+		0, 5, 1,
+		3, 7, 2,	// side 6
+		2, 7, 6
+	};
+	
+	// create an index buffer interface called i_buffer
+	d3ddev->CreateIndexBuffer( 36 * sizeof(short), // 3 per triangle, 12;
+								0,
+								D3DFMT_INDEX16,
+								D3DPOOL_MANAGED,
+								&i_buffer,
+								NULL);
+
+	// lock i_buffer and load the indices into it
+	i_buffer->Lock(0, 0, (void**)&pVoid, 0);
+	memcpy(pVoid, indices, sizeof(indices));
+	i_buffer->Unlock();
 }
